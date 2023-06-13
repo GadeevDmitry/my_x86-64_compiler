bf44c3eacf0aa5e1535c126ed0d20f50a57cf577
Gadeev Dmitry <gadeev.dima@mail.ru>
2023-06-13 14:02:14 +0300

    fix little buf + add Makefile in tokenizer

--- frontend/tokenizer/tokenizer_static.h
+++ frontend/tokenizer/tokenizer_static.h
@@ -1,7 +1,21 @@
 #ifndef TOKENIZER_STATIC_H
 #define TOKENIZER_STATIC_H
 
-#include "../common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define LOG_NTRACE
+#define LOG_NLEAK
+#define LOG_NVERIFY
+#define LOG_NDEBUG
+
+#include "../../lib/logs/log.h"
+#include "../../lib/algorithm/algorithm.h"
+#include "../../lib/stack/stack.h"
+#include "../../lib/vector/vector.h"
+
 #include "tokenizer.h"
 
 //================================================================================================================================
